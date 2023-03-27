3389f58c6f5e41015fb33e00be2f31b00287b583
joaoqueiroga <queiroga.joaofcsol@gmail.com>
2023-03-27 15:43:57 -0300

    Criadas classe aresta

--- codigo/src/Aresta.java
+++ codigo/src/Aresta.java
@@ -1,2 +1,30 @@
 public class Aresta {
+    private int destino;
+    private boolean visitada = false;
+    private int peso;
+
+    public Aresta(int destino, int peso) {
+        this.destino = destino;
+        this.peso = peso;
+    }
+
+    public int destino() {
+        return destino;
+    }
+
+    public boolean visitada() {
+        return visitada;
+    }
+
+    public int peso() {
+        return peso;
+    }
+
+    public void visitar() {
+        visitada = true;
+    }
+
+    public void limparVisita() {
+        visitada = false;
+    }
 }
