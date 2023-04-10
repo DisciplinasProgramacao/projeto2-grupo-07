74c3a1e4f0e838cee08d738f05dd337416bc942f
joaoqueiroga <queiroga.joaofcsol@gmail.com>
2023-04-10 19:28:24 -0300

    adicionado método clear console

--- codigo/src/App.java
+++ codigo/src/App.java
@@ -1,7 +1,22 @@
 public class App {
 
       
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         System.out.println("Hello World");
+        clearConsole();
+    }
+
+    //Does not work on IDE Console
+    public static void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            }
+            else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (Exception ignored) {}
     }
 }
